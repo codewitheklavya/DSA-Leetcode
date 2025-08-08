fadf273ba2f36863d22e8464989c3c4d06ba6924
eklavyasingh <codewitheklavya@gmail.com>
2025-08-08 22:01:08 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (79.16%) - LeetSync

--- 54-spiral-matrix/spiral-matrix.cpp
+++ 54-spiral-matrix/spiral-matrix.cpp
@@ -2,40 +2,35 @@ class Solution {
 public:
     vector<int> spiralOrder(vector<vector<int>>& matrix) {
         int m = matrix.size();
-        int n = matrix[0].size();    
-        int minr = 0, minc = 0;
-        int maxr = m - 1, maxc = n - 1;
+        int n = matrix[0].size();
+        int minr = 0; int minc = 0;
+        int maxr = m-1; int maxc = n-1;
         vector<int> v;
-
-        while (minr <= maxr && minc <= maxc) {
-            // right
-            for (int j = minc; j <= maxc; j++) {
-                v.push_back(matrix[minr][j]);
+        while(minr<=maxr && minc<=maxc){
+            //right
+            for(int i=minc;i<=maxc;i++){
+                v.push_back(matrix[minr][i]);
             }
             minr++;
-            if (minr > maxr || minc > maxc) break;
-
-            // down
-            for (int i = minr; i <= maxr; i++) {
-                v.push_back(matrix[i][maxc]);
+            if(minr>maxr || minc>maxc) break;
+            //down
+            for(int j=minr;j<=maxr;j++){
+                v.push_back(matrix[j][maxc]);
             }
             maxc--;
-            if (minr > maxr || minc > maxc) break;
-
-            // left
-            for (int j = maxc; j >= minc; j--) {
-                v.push_back(matrix[maxr][j]);
+            //left
+            if(minr>maxr || minc>maxc) break;
+            for(int i=maxc;i>=minc;i--){
+                v.push_back(matrix[maxr][i]);
             }
             maxr--;
-            if (minr > maxr || minc > maxc) break;
-
-            // up
-            for (int i = maxr; i >= minr; i--) {
-                v.push_back(matrix[i][minc]);
+            //up
+            if(minr>maxr || minc>maxc) break;
+            for(int j=maxr;j>=minr;j--){
+                v.push_back(matrix[j][minc]);
             }
             minc++;
         }
-
         return v;
     }
 };
