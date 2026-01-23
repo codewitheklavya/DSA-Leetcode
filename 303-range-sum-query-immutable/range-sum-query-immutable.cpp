ed5d72b2017cae385212b4402d8a21e135c53938
eklavyasingh <codewitheklavya@gmail.com>
2026-01-23 22:27:04 +0530

    Time: 3 ms (47.56%) | Memory: 24 MB (60.70%) - LeetSync

---
+++ 303-range-sum-query-immutable/range-sum-query-immutable.cpp
@@ -0,0 +1,16 @@
+class NumArray {
+private:
+    vector<int> prefix;
+
+public:
+    NumArray(vector<int>& nums) {
+        prefix.push_back(0);  // prefix[0] = 0
+        for (int num : nums) {
+            prefix.push_back(prefix.back() + num);
+        }
+    }
+
+    int sumRange(int left, int right) {
+        return prefix[right + 1] - prefix[left];
+    }
+};
