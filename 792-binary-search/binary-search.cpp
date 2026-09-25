f12c9755154425f87ccf7d8c95286a7c77736664
eklavyasingh <codewitheklavya@gmail.com>
2026-09-25 13:48:09 +0530

    Time: 0 ms (100.00%) | Memory: 31.4 MB (45.53%) - LeetSync

--- 792-binary-search/binary-search.cpp
+++ 792-binary-search/binary-search.cpp
@@ -3,16 +3,14 @@ public:
     int search(vector<int>& nums, int target) {
         int low = 0;
         int hi = nums.size()-1;
-        int mid = (low+hi)/2;
         while(low<=hi){
+            int mid = low+(hi-low)/2;
             if(nums[mid]==target) return mid;
             if(nums[mid]<target){
                 low = mid+1;
-                mid = (low+hi)/2;
             }
             else if(nums[mid]>target){
                 hi = mid-1;
-                mid = (low+hi)/2;
             }
         }
         return -1;
